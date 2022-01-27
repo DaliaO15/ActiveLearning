adfe363bb9dec47ce46e740c3a0ac499dcdbfe5e
DaliaO15 <dalia141503@gmail.com>
2022-01-27 15:31:28 +0100

    Playing with modAL

--- ALexample1/try_gif.py
+++ ALexample1/try_gif.py
@@ -2,5 +2,5 @@ from IPython import display
 from IPython.display import Image
 
 # Display GIF in Jupyter, CoLab, IPython
-with open("rs1it5.gif",'rb') as f:
+with open("rs1it5.gif", 'rb') as f:
     display.Image(data=f.read(), format='png')
